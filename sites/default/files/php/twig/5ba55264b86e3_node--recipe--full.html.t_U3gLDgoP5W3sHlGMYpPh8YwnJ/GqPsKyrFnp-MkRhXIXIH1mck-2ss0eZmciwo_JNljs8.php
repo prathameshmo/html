3959929ab6c0e1795a1f948fbac8c644043d6a02
EC2 Default User <ec2-user@ip-172-31-1-102.ap-south-1.compute.internal>
2018-09-26 06:09:05 +0000

    commit from tar

---
+++ sites/default/files/php/twig/5ba55264b86e3_node--recipe--full.html.t_U3gLDgoP5W3sHlGMYpPh8YwnJ/GqPsKyrFnp-MkRhXIXIH1mck-2ss0eZmciwo_JNljs8.php
@@ -0,0 +1,139 @@
+<?php
+
+/* core/profiles/demo_umami/themes/umami/templates/content/node--recipe--full.html.twig */
+class __TwigTemplate_738d6afb9aee3fa4ddf3ba00a07051eec222b8dc70c4a26393b55f023fcff4e4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 70, "if" => 91, "trans" => 94);
+        $filters = array("clean_class" => 72, "format_date" => 79);
+        $functions = array("attach_library" => 80);
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set', 'if', 'trans'),
+                array('clean_class', 'format_date'),
+                array('attach_library')
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 70
+        $context["classes"] = array(0 => "node", 1 => ("node--type-" . \Drupal\Component\Utility\Html::getClass($this->getAttribute(        // line 72
+($context["node"] ?? null), "bundle", array()))), 2 => (($this->getAttribute(        // line 73
+($context["node"] ?? null), "isPromoted", array(), "method")) ? ("node--promoted") : ("")), 3 => (($this->getAttribute(        // line 74
+($context["node"] ?? null), "isSticky", array(), "method")) ? ("node--sticky") : ("")), 4 => (( !$this->getAttribute(        // line 75
+($context["node"] ?? null), "isPublished", array(), "method")) ? ("node--unpublished") : ("")), 5 => ((        // line 76
+($context["view_mode"] ?? null)) ? (("node--view-mode-" . \Drupal\Component\Utility\Html::getClass(($context["view_mode"] ?? null)))) : ("")));
+        // line 79
+        $context["created_date"] = call_user_func_array($this->env->getFilter('format_date')->getCallable(), array($this->getAttribute(($context["node"] ?? null), "getCreatedTime", array()), "umami_dates"));
+        // line 80
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->attachLibrary("classy/node"), "html", null, true));
+        echo "
+<article";
+        // line 81
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["attributes"] ?? null), "addClass", array(0 => ($context["classes"] ?? null)), "method"), "html", null, true));
+        echo ">
+
+  ";
+        // line 83
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title_prefix"] ?? null), "html", null, true));
+        echo "
+    <header class=\"node__header\">
+      <h1 class=\"page-title\">
+        ";
+        // line 86
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["label"] ?? null), "html", null, true));
+        echo "
+      </h1>
+    </header>
+  ";
+        // line 89
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title_suffix"] ?? null), "html", null, true));
+        echo "
+
+  ";
+        // line 91
+        if (($context["display_submitted"] ?? null)) {
+            // line 92
+            echo "  <footer class=\"node__meta\">
+    <div";
+            // line 93
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["author_attributes"] ?? null), "addClass", array(0 => "node__submitted"), "method"), "html", null, true));
+            echo ">
+      ";
+            // line 94
+            echo t("<span class=\"by-author\">by @author_name</span> @created_date", array("@author_name" => ($context["author_name"] ?? null), "@created_date" => ($context["created_date"] ?? null), ));
+            // line 95
+            echo "      ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["metadata"] ?? null), "html", null, true));
+            echo "
+    </div>
+  </footer>
+  ";
+        }
+        // line 99
+        echo "
+  <div";
+        // line 100
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content_attributes"] ?? null), "addClass", array(0 => "node__content"), "method"), "html", null, true));
+        echo ">
+    ";
+        // line 101
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true));
+        echo "
+  </div>
+
+</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/profiles/demo_umami/themes/umami/templates/content/node--recipe--full.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  104 => 101,  100 => 100,  97 => 99,  89 => 95,  87 => 94,  83 => 93,  80 => 92,  78 => 91,  73 => 89,  67 => 86,  61 => 83,  56 => 81,  52 => 80,  50 => 79,  48 => 76,  47 => 75,  46 => 74,  45 => 73,  44 => 72,  43 => 70,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/profiles/demo_umami/themes/umami/templates/content/node--recipe--full.html.twig", "/var/www/html/core/profiles/demo_umami/themes/umami/templates/content/node--recipe--full.html.twig");
+    }
+}
